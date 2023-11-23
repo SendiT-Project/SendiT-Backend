a1fc40203b7eeefae55e4cf53d7c5b66c823e61b
Medrine Mulindi <medrine.mulindi@gmail.com>
2023-11-23 21:43:56 +0300

    feat:deployment

--- app/app.py
+++ app/app.py
@@ -2,10 +2,14 @@
 from flask import Flask
 from flask_migrate import Migrate
 from models import db, User,Tracking,Order
+import os
+from dotenv import load_dotenv
 
+load_dotenv()
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"]='sqlite:///app.db'
+app.secret_key = os.environ("SECRET_KEY")
+app.config["SQLALCHEMY_DATABASE_URI"]= os.environ("DATABASE_URI")
 app.config["SQLACHEMY_TRACK_MODIFICATIONS"]=False
 
 migrate = Migrate(app,db)
