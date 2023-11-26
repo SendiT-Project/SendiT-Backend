f67318f8ada6942596ea540df49e54dc5a151fa6
cynthiawuor <cynthiawuor254@gmail.com>
2023-11-26 22:49:00 +0300

    readme

--- README.md
+++ README.md
@@ -1,1 +1,84 @@
 # SendiT-Backend
+# Flask User Authentication and Order Tracking App
+
+This is a Flask application designed for user authentication and order tracking. It provides a secure user registration and login system, allowing users to place orders and track their status.
+
+## Table of Contents
+
+- [Features](#features)
+- [Prerequisites](#prerequisites)
+- [Installation](#installation)
+- [Configuration](#configuration)
+- [Usage](#usage)
+- [Endpoints](#endpoints)
+- [Database Seeding](#database-seeding)
+- [Testing](#testing)
+- [Contributing](#contributing)
+- [License](#license)
+
+## Features
+
+- User Registration and Authentication
+- Secure Password Hashing
+- Order Placement and Tracking
+- Session Management
+- API Endpoints for User and Order Information
+
+## Prerequisites
+
+Before you begin, ensure you have the following prerequisites installed on your machine:
+
+- Python 3.x
+- pip (Python package installer)
+- [Virtualenv](https://virtualenv.pypa.io/en/latest/) (recommended)
+
+## Installation
+
+1. Clone the repository:
+
+   ```bash
+   git clone https://github.com/your-username/flask-auth-order-tracking.git
+   cd flask-auth-order-tracking
+
+2. Create and activate a virtual environment (optional but recommended):
+venv\Scripts\activate
+On macOS and Linux:
+source venv/bin/activate
+
+3. Install dependencies:
+pip install -r requirements.txt
+
+4. Set up environment variables:
+Create a .env file in the root directory and add the following:
+SECRET_KEY=your_secret_key
+DATABASE_URI=your_database_uri
+Replace your_secret_key and your_database_uri with your actual secret key and database URI.
+
+DATABASE MIGRATION:
+Run the following commands to apply the database migrations:
+flask db init
+flask db migrate
+flask db upgrade
+
+USAGE
+Run the application:
+python your_app_filename.py
+
+By default, the application runs in debug mode. Open your web browser and go to http://localhost:5000 to access the app.
+
+CONFIGURATION
+Secret Key: The SECRET_KEY is a crucial security measure for Flask applications. Ensure it's kept secret and unique for your application.
+
+Database URI: Configure the DATABASE_URI with the appropriate connection details for your database (e.g., SQLite, PostgreSQL).
+
+ENDPOINTS
+/: Home endpoint.
+/signup: User registration endpoint (POST request).
+/login: User login endpoint (POST request).
+/logout: User logout endpoint (DELETE request).
+/session: Check user session (GET request).
+Database Seeding
+To populate the database with sample data, run the seeding script:
+python seed.py
+This script adds dummy users, orders, and tracking data to the database.
+
--- app/app.py
+++ app/app.py
@@ -1,20 +1,103 @@
 
-from flask import Flask
+from flask import Flask, make_response, jsonify, session, request
+from flask_restful import Api, Resource
 from flask_migrate import Migrate
-from models import db, User,Tracking,Order
+from models import db, User,Order
 import os
 from dotenv import load_dotenv
 
 load_dotenv()
 
 app = Flask(__name__)
-app.secret_key = os.environ("SECRET_KEY")
-app.config["SQLALCHEMY_DATABASE_URI"]= os.environ("DATABASE_URI")
+app.secret_key = os.environ["SECRET_KEY"]
+app.config["SQLALCHEMY_DATABASE_URI"]= os.environ["DATABASE_URI"]
 app.config["SQLACHEMY_TRACK_MODIFICATIONS"]=False
 
 migrate = Migrate(app,db)
 db.init_app(app)
 
+api = Api(app)
+
+@app.before_request
+def check_if_logged_in():
+
+    if 'user_id' not in session and request.endpoint not in ["login", "signup","index"]:
+        return {"error": "unauthorized"}, 401
+
+class CheckSession(Resource):
+    def get(self):
+        if session.get('user_id'):
+            user = User.query.filter(User.id==session['user_id']).first()
+            return user.to_dict(), 200
+        
+        return {'error': 'Resource unavailable'}
+    
+
+class Index(Resource):
+    def get(self):
+        response_body = "<h1>Hello, World</h1>"
+        status =200
+        headers ={}
+        return make_response(response_body, status, headers)
+
+
+class Signup(Resource):
+    def post(self):
+        username = request.get_json().get("username")
+        email = request.get_json().get("email")
+        password = request.get_json().get("password")
+
+        if username and email and password:
+            new_user = User(username=username, email=email)
+            new_user.password_hash = password
+
+            db.session.add(new_user)
+            db.session.commit()
+
+            session["user_id"] = new_user.id
+            return new_user.to_dict(), 201
+        
+        return {"error": "user details must be added"}, 422
+        
+
+class Login(Resource):
+    def post(self):
+        username = request.get_json().get("username")
+        password = request.get_json().get("password")
+        user = User.query.filter(User.username==username).first()
+
+        if user:
+            if user.authenticate(password):
+                session['user_id'] = user.id
+
+                user_dict = user.to_dict()
+                print("Login successful. user ID:", user.id)  
+                return make_response(jsonify(user_dict), 201)
+            else:
+                print("Invalid password.")  
+                return {"error": "Invalid password"}, 401
+        
+        print("User not registered.") 
+        return {"error": "User not Registered"}, 404
+        
+
+class Logout(Resource):
+    def delete(self):
+        if session.get('user_id'):
+            session['user_id'] = None
+            return {'info': 'user logged out successfully'}
+        else:
+            return {'error': 'unauthorized'}, 401
+        
+
+api.add_resource(CheckSession, "/session", endpoint="session")
+api.add_resource(Index, "/", endpoint="index") 
+api.add_resource(Signup, "/signup", endpoint="signup")
+api.add_resource(Login, "/login", endpoint="login")       
+api.add_resource(Logout, "/logout", endpoint="logout")
+
+
+application = app
+
 if __name__ == '__main__':
     app.run(debug=True)
-
