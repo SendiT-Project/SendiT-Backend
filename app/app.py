5688c1541b6e4881b4a97dc1c64329a595ec6be5
Medrine Mulindi <medrine.mulindi@gmail.com>
2023-12-01 19:03:18 +0300

    changes

--- app/app.py
+++ app/app.py
@@ -1,7 +1,10 @@
 
 from flask import Flask, make_response, jsonify, session, request
 from flask_restful import Api, Resource
+from datetime import timedelta
 from flask_migrate import Migrate
+from flask_cors import CORS
+from flask_session import Session
 from models import db, User,Order, Admin
 from werkzeug.exceptions import NotFound
 import os
@@ -14,17 +17,24 @@ app = Flask(__name__)
 app.secret_key = os.environ["SECRET_KEY"]
 app.config["SQLALCHEMY_DATABASE_URI"]= os.environ["DATABASE_URI"]
 app.config["SQLACHEMY_TRACK_MODIFICATIONS"]=False
+app.config['SESSION_TYPE'] = 'filesystem'
+app.config["PERMANENT_SESSION_LIFETIME"] = timedelta(days=1)
+app.config['SESSION_COOKIE_SAMESITE'] = 'Lax'
+app.config['SESSION_FILE_DIR'] = 'session_dir'
 
 migrate = Migrate(app,db)
 db.init_app(app)
 
 api = Api(app)
+app.config.from_object(__name__)
+Session(app)
+CORS(app, origins="*")
 
-# @app.before_request
-# def check_if_logged_in():
+@app.before_request
+def check_if_logged_in():
 
-#     if 'user_id' not in session and request.endpoint not in ["login", "signup","index"]:
-#         return {"error": "unauthorized"}, 401
+    if 'user_id' not in session and request.endpoint not in ["login", "signup", "session", "index"]:
+        return {"error": "unauthorized"}, 401
 
     
 
@@ -62,6 +72,9 @@ class Login(Resource):
         user = User.query.filter(User.username==username).first()
 
         if user:
+            # if "user_id" in session:
+            #     return {"error": "User is already logged in"}, 400
+            
             if user.authenticate(password):
                 session['user_id'] = user.id
 
